a435a961d7c29348a58ae1c331bce87f82a8ff73
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-02-21 23:27:27 -0500

    a whole lot of stuff pretty much made whole prototype

--- Engine.js
+++ Engine.js
@@ -2,17 +2,39 @@
 
 
 let uniqueId = 0;
+class Vector2 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
 
+    }
+}
 class Scene {
     constructor(canvas) {
+
+        this.cameraPos = new Vector2(-540, -540);
+        this.cameraCast = 1080;
+
         this.objects = [];
         this.ctx = canvas.getContext('2d');
+        this.width = 1080;
+        this.height = 800;
         
     }
     Update() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        var c = this.ctx;
+        //drawing objects
+        var camPos = this.cameraPos;
+        var camCast = this.cameraCast;
         this.objects.forEach(function (obj) {
-            obj.draw();
+            obj.Draw(c, (obj.x - 540) * camCast / 1080 - camPos.x, (obj.y - 540) * camCast / 1080 - camPos.y, camCast / 1080);
         })
+    }
+    CameraPosition(pos) {
+        var x = (pos.x - 540) * this.cameraCast / 1080 - this.cameraPos.x;
+        var y = (pos.y - 540) * this.cameraCast / 1080 - this.cameraPos.y;
+        return new Vector2(x, y);
     }
     GetObject(id) {
         this.objects.forEach(function (obj) {
@@ -21,12 +43,34 @@ class Scene {
             }
         });
     }
+    GetAllObjectsOfName(name) {
+        var objs = []
+        this.objects.forEach(function (obj) {
+            if (obj.name == name) {
+                objs.push(obj);
+            }
+        });
+        return objs;
+    }
     AddObject(object) {
         this.objects.push(object);
     }
+    CreateObject(name) {
+        var newObject = new Object(name, uniqueId);
+        this.AddObject(newObject);
+        uniqueId++;
+        return newObject;
+    }
+    DestroyObject(object) {
+        
+        this.objects = this.objects.filter(function (item) {
+            return item !== object;
+        });
+    }
 
 }
-let scene = new Scene(const canvas = document.getElementById('canvas'));
+
+let scene = new Scene(document.getElementById('canvas'));
 
 class Object {
     constructor(name, uniqueId) {
@@ -40,54 +84,97 @@ class Object {
         this.rotation = 0;
         this.components = [];
     }
-    draw() {
-        this.drawRotatedRect(this.x, this.y, this.width, this.height, this.rotation)
+    Draw(context, x, y, scale) {
+        DrawRect(context, x, y, this.width * scale, this.height * scale, this.rotation);
     }
-    drawRotatedRect(x, y, width, height, degrees) {
-
-        // first save the untranslated/unrotated context
-        ctx.save();
-
-        ctx.beginPath();
-        // move the rotation point to the center of the rect
-        ctx.translate(x + width / 2, y + height / 2);
-        // rotate the rect
-        ctx.rotate(degrees * Math.PI / 180);
-
-        // draw the rect on the transformed context
-        // Note: after transforming [0,0] is visually [x,y]
-        //       so the rect needs to be offset accordingly when drawn
-        ctx.rect(-width / 2, -height / 2, width, height);
-
-        ctx.fill();
-
-        // restore the context to its untranslated/unrotated state
-        ctx.restore();
-
-    }
-}
-
-class Vector2 {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-    }
-}
-class Vector3 {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+    
 }
 
 
-function CreateObject(name) {
-    var newObject = new Object(name, uniqueId);
-    scene.AddObject(newObject);
-    uniqueId++;
-    return newObject;
+
+//Drawing Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Drawing Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Drawing Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function DrawRect(context, x, y, width, height, degrees, color) {
+
+    x -= width / 2;
+    y -= height / 2;
+    // first save the untranslated/unrotated context
+    context.save();
+    context.fillStyle = color;
+    context.beginPath();
+    // move the rotation point to the center of the rect
+    context.translate(x + width / 2, y + height / 2);
+    // rotate the rect
+    context.rotate(degrees * Math.PI / 180);
+
+
+    // draw the rect on the transformed context
+    // Note: after transforming [0,0] is visually [x,y]
+    //       so the rect needs to be offset accordingly when drawn
+    context.rect(-width / 2, -height / 2, width, height);
+
+    context.fill();
+
+    // restore the context to its untranslated/unrotated state
+    context.restore();
+
 }
+function DrawCircle(context, x, y ,radius, color) {
+
+    context.save();
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = '';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = '#000000';
+    context.stroke();
+    context.restore();
+}
+function DrawLine(context, startX, startY, endX, endY) {
+    context.save();
+    context.lineWidth = 10;
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.stroke();
+    context.restore();
+}
+//Math Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Math Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Math Functions//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Max(num1, num2) {
+    if (num1 > num2) return num1;
+    return num2;
+}
+function Min(num1, num2) {
+    if (num1 < num2) return num1;
+    return num2;
+}
+function RGB(r, b, g) {
+    return "RGB(" + r+ "," + b + "," + g + ")";
+}
+//Vector Math
+function DirectionVector(angle) {
+    var x = Math.cos(angle * Math.PI / 180);
+    var y = Math.sin(angle * Math.PI / 180);
+    return new Vector2(x, y);
+}
+function MultiplyVector(vector, magnitude) {
+    vector.x *= magnitude;
+    vector.y *= magnitude;
+    return vector;
+}
+function OrganicDistanceVector(vector1, vector2) {
+   return Math.sqrt(Math.pow(vector1.x - vector2.x, 2), Math.pow(vector1.y - vector2.y, 2));
+}
+function DistanceVector(vector1, vector2) {
+    return Math.sqrt(Math.pow(vector1.x - vector2.x, 2)+ Math.pow(vector1.y - vector2.y, 2));
+
+}
+
 
 
 
--- Simulator.js
+++ Simulator.js
@@ -1,5 +1,429 @@
+const frameRate = 20;
+//editor
+let parameterCurveRamping = 2;
+let mutationRate = 5;
+let organicWander = "off";
+let plantSpawnRate = 100;
+let isPlantSpawnRate = "on";
+let plantNumber = 100;
+let isPlantNumber = undefined;
+let showTargetLines = undefined;
+
+let speedMetabolism = [1, 2];
+let agilityMetabolism = [1, 2];
+let sizeMetabolism = [1, 2];
+let storageMetabolism = [1, 2];
+let sightAngleMetabolism = [1, 2];
+let sightLengthMetabolism = [1, 2];
+
+
+let frameCounter = 0;
+
+function Start() {
+
+}
+function Update() {
+    frameCounter++;
+    DisplayAverages();
+
+    var things = AllThings();
+    things.forEach(function (thing) {
+        thing.Move();
+        thing.Turn();
+        thing.Eat();
+        thing.Metabolize();
+    })
+    var eggs = AllEggs();
+    eggs.forEach(function (egg) {
+        egg.Incubate();
+    })
+
+    //plants
+    if (frameCounter % plantSpawnRate == 0 & isPlantSpawnRate == "on") {
+        var plant = new Plant(100, 10);
+    }
+    if (scene.GetAllObjectsOfName("plant").length < plantNumber & isPlantNumber == "on") {
+        new Plant(100, 10);
+    }
+}
+//EDITOR FUNCTIONS
+function Restart() {
+    scene.objects = [];
+    var thing = new Thing(100, 100, 100, 100, 100, 100);
+    frameCounter = 0;
+
+}
+function ApplyChanges(){
+    parameterCurveRamping = parseFloat($("#curveRamping").val());
+    organicWander = $("#organicWander").val();
+    plantSpawnRate = $("#plantSpawnRate").val();
+    isPlantSpawnRate = $("#usePlantSpawnRate:checked").val();
+    plantNumber = $("#plantNumber").val();
+    isPlantNumber = $("#usePlantNumber:checked").val();
+    organicWander = $("#organicWander:checked").val();
+    showTargetLines = $("#showTargetLines:checked").val();
+
+    mutationRate = $("#mutationRate").val();
+
+    speedMetabolism = $("#speedMetabolism").val().split("x^");
+    agilityMetabolism = $("#agilityMetabolism").val().split("x^");
+    sizeMetabolism = $("#sizeMetabolism").val().split("x^");
+    storageMetabolism = $("#storageMetabolism").val().split("x^");
+    sightAngleMetabolism = $("#sightAngleMetabolism").val().split("x^");
+    sightLengthMetabolism = $("#sightLengthMetabolism").val().split("x^");
+}
+function CreateThing() {
+    tSpeed = parseFloat($("#tSpeed").val());
+    tAgility = parseFloat($("#tAgility").val());
+    tSize = parseFloat($("#tSize").val());
+    tStorage = parseFloat($("#tStorage").val());
+    tSightAngle = parseFloat($("#tSightAngle").val());
+    tSightLength = parseFloat($("#tSightLength").val());
+    tLifeTime = parseFloat($("#tLifetime").val());
+    new Thing(tSpeed, tAgility, tSize, tStorage, tSightAngle, tSightLength);
+}
+
+
+
+
+
+//Game Stuff
+class Thing extends Object {
+    constructor(speed, agility, size, storage, sightAngle, sightLength, lifeTime) {
+        super();
+        this.energy = storage / 2;
+
+        this.speed = speed;
+        this.agility = agility;
+        this.size = size;
+        this.storage = storage;
+        this.sightAngle = sightAngle;
+        this.sightLength = sightLength;
+        this.lifeTime = lifeTime;
+
+        var sm = speedMetabolism[0] * Math.pow(speed, speedMetabolism[1]);
+        var am = agilityMetabolism[0] * Math.pow(agility, agilityMetabolism[1]);
+        var zm = sizeMetabolism[0] * Math.pow(size, sizeMetabolism[1]);
+        var tm = storageMetabolism[0] * Math.pow(storage, storageMetabolism[1]);
+        var sam = sightAngleMetabolism[0] * Math.pow(sightAngle, sightAngleMetabolism[1]);
+        var slm = sightLengthMetabolism[0] * Math.pow(sightLength, sightLengthMetabolism[1]);
+        this.metabolism = (sm + am + zm + tm + sam + slm) / 3600000;
+        console.log(this.metabolism);
+
+        let pos = RandomScreenPosition();
+        this.x = pos.x;
+        this.y = pos.y;
+        this.direction = Math.random() * 360;
+        this.name = "thing";
+        scene.AddObject(this);
+
+
+    }
+    Draw(context, x, y, scale) {
+        var localDirectionVectorEyeLeft = MultiplyVector(DirectionVector(this.direction - Curve(this.sightAngle) / 2), this.size / 5 * scale)
+        var localDirectionVectorEyeRight = MultiplyVector(DirectionVector(this.direction + Curve(this.sightAngle) / 2), this.size / 5 * scale)
+        var localDirectionVectorSightLeft = MultiplyVector(DirectionVector(this.direction - Curve(this.sightAngle) / 2), Curve(this.sightLength) / 2 * scale);
+        var localDirectionVectorSightRight = MultiplyVector(DirectionVector(this.direction + Curve(this.sightAngle) / 2), Curve(this.sightLength) / 2 * scale);
+        var color = RGB(this.speed, this.agility, 0);
+        //target
+        if (this.NearestPlant() != null & showTargetLines == "on") {
+            DrawLine(context, x, y, scene.CameraPosition(this.NearestPlant()).x, scene.CameraPosition(this.NearestPlant()).y);
+        }
+        //sight
+        DrawRect(context, x + localDirectionVectorSightLeft.x, y + localDirectionVectorSightLeft.y, Curve(this.sightLength) * scale, 3 * scale, this.direction - Curve(this.sightAngle) / 2, color);
+        DrawRect(context, x + localDirectionVectorSightRight.x, y + localDirectionVectorSightRight.y, Curve(this.sightLength) * scale, 3 * scale, this.direction + Curve(this.sightAngle) / 2, color);
+        //body
+        DrawCircle(context, x, y, Curve(this.size) / 3 * scale, color);
+        //eyes
+        DrawCircle(context, x + localDirectionVectorEyeLeft.x, y + localDirectionVectorEyeLeft.y, Curve(this.size) / 15 *scale, "white");
+        DrawCircle(context, x + localDirectionVectorEyeRight.x, y + localDirectionVectorEyeRight.y, Curve(this.size) / 15 * scale, "white");
+        //energy bar
+        DrawRect(context, x, y - this.size / 2, this.storage, 10, 0, "black");
+        DrawRect(context, x + this.energy / 2 - this.storage/2, y - this.size / 2, this.energy * scale, 10 * scale, 0, "red");
+        
+    }
+
+    Move() {
+        var directionVectorMovement = MultiplyVector(DirectionVector(this.direction), Curve(this.speed) / 1000);
+        this.x += directionVectorMovement.x;
+        this.y += directionVectorMovement.y;
+    }
+    Eat() {
+        var plant = this.NearestPlant();
+        if (plant != null) {
+            if (DistanceVector(new Vector2(this.x, this.y), new Vector2(plant.x, plant.y)) < Curve(this.size) / 3 + plant.size / 3) {
+                this.energy += plant.energy;
+                if (this.energy > this.storage) {
+                    this.energy /= 2;
+                    this.Reproduce();
+                }
+                plant.Eaten();
+            }
+        }
+    }
+    Reproduce() {
+        
+        new Egg(this.x, this.y,this.speed, this.agility, this.size, this.storage, this.sightAngle, this.sightLength);
+    }
+    NearestPlant() {
+        
+        var plants = scene.GetAllObjectsOfName("plant");
+
+        var closestDistance = Infinity;
+        var closestPlant = null;
+        var pos = new Vector2(this.x, this.y);
+        plants.forEach(function (plant) {
+            if (organicWander == "on") {
+                var distance = OrganicDistanceVector(new Vector2(plant.x, plant.y), new Vector2(pos.x, pos.y));
+            } else {
+                var a = plant.x - pos.x;
+                var b = plant.y - pos.y;
+
+                var distance = Math.sqrt(a * a + b * b);
+            }
+            
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestPlant = plant;
+            }
+        });
+        return closestPlant;
+    }
+    NearestPlantAngle() {
+        var target = this.NearestPlant();
+        
+        var angle = Math.atan2(target.y - this.y, target.x - this.x) / Math.PI * 180 - (this.direction);
+        return angle;
+    }
+    IsTurnDirectionRight() {
+        //TRUE IS RIGHT
+        //FALSE IS LEFT
+
+        var npa = this.NearestPlantAngle();
+        if (npa > 0 || (npa > -360 && npa < -180)) {
+            return true;
+        }
+        return false
+
+    }
+    Turn() {
+        if (this.NearestPlant() != null) {
+            if (this.IsTurnDirectionRight()) {
+                this.direction += Curve(this.agility) / 1000;
+            } else {
+                this.direction -= Curve(this.agility) / 1000;
+            }
+        }
+    }
+    Metabolize() {
+        var sm = speedMetabolism[0] * Math.pow(this.speed, speedMetabolism[1]);
+        var am = agilityMetabolism[0] * Math.pow(this.agility, agilityMetabolism[1]);
+        var zm = sizeMetabolism[0] * Math.pow(this.size, sizeMetabolism[1]);
+        var tm = storageMetabolism[0] * Math.pow(this.storage, storageMetabolism[1]);
+        var sam = sightAngleMetabolism[0] * Math.pow(this.sightAngle, sightAngleMetabolism[1]);
+        var slm = sightLengthMetabolism[0] * Math.pow(this.sightLength, sightLengthMetabolism[1]);
+        this.metabolism = (sm + am + zm + tm + sam + slm) / 3600000;
+
+        this.DepleteEnergy(this.metabolism);
+        if (this.energy > 0) return;
+
+        this.Die();
+    }
+    Die() {
+        scene.DestroyObject(this);
+    }
+    DepleteEnergy(amount) {
+        this.energy -= amount;
+    }
+
+    
+}
+function AllThings() {
+    var things = scene.GetAllObjectsOfName("thing");
+    return things;
+}
+class Egg extends Object {
+    constructor(x, y, speed, agility, size, storage, sightAngle, sightLength, lifeTime) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.speed = this.Mutate(speed);
+        this.agility = this.Mutate(agility);
+        this.size = this.Mutate(size);
+        this.storage = this.Mutate(storage);
+        this.sightAngle = this.Mutate(sightAngle);
+        this.sightLength = this.Mutate(sightLength);
+        this.lifeTime = this.Mutate(lifeTime)
+
+        this.framesLeft = 1000;
+        
+        this.name = "egg"
+        scene.AddObject(this);
+    }
+    Mutate(parameter) {
+        return parameter += (Math.random() - 0.5) * 2 * mutationRate;
+    }
+    Incubate() {
+        this.framesLeft--;
+        if (this.framesLeft <= 0) {
+            var thing = new Thing(this.speed, this.agility, this.size, this.storage, this.sightAngle, this.sightLength, this.lifeTime);
+            thing.x = this.x;
+            thing.y = this.y;
+            scene.DestroyObject(this);
+        }
+    }
+    Draw(context, x, y, scale) {
+        DrawRect(context, x, y, this.width / 9 * scale, this.height / 9 * scale, 0, RGB(this.speed, this.agility));
+    }
+    
+}
+function AllEggs() {
+    var things = scene.GetAllObjectsOfName("egg");
+    return things;
+}
+class Plant extends Object {
+    constructor(size, energy) {
+        super();
+        this.size = size;
+        this.energy = energy;
+
+        let pos = RandomScreenPosition();
+        this.x = pos.x;
+        this.y = pos.y;
+        this.name = "plant";
+        scene.AddObject(this);
+    }
+    Draw(context, x, y, scale) {
+        DrawRect(context, x, y, this.size / 3 * scale, this.size / 3 * scale, 0);
+    }
+    Eaten() {
+        scene.DestroyObject(this);
+    }
+}
+function RandomScreenPosition() {
+    return new Vector2(Math.random() * 1080, Math.random() * 800);
+}
+//EVOLUTION FUNCTIONS////////////////////////////////////////////////////////////////////////////////
+//EVOLUTION FUNCTIONS////////////////////////////////////////////////////////////////////////////////
+//EVOLUTION FUNCTIONS////////////////////////////////////////////////////////////////////////////////
+function Curve(parameter) {
+    return Math.pow(parameter * Math.pow(10, parameterCurveRamping - 2) / Math.pow(10, parameterCurveRamping), parameterCurveRamping) * 100;
+}
+//////////////////////////////////////////////////////EDITOR FUNCTIONS////////////////////////////////
+function DisplayAverages() {
+    var params = AverageParameters();
+    $("#averages").html("<b>Averages</b><br/>Speed: " + params[0] + " <br/>Agility: " + params[1] + " <br/>Size: " + params[2] + " <br/>Storage: " + params[3] + " <br/>SightAngle: " + params[4] + " <br/>SightLength: " + params[5] + " <br/>");
+}
+function AverageParameters() {
+    
+    things = AllThings();
+    if (things.length == 0) {
+        return [0, 0, 0, 0, 0, 0];
+    }
+    speed = 0;
+    agility = 0;
+    size = 0;
+    storage = 0;
+    sightAngle = 0;
+    sightLength = 0;
+    things.forEach(function (thing) {
+        speed += thing.speed;
+        agility += thing.agility;
+        size += thing.size;
+        storage += thing.storage;
+        sightAngle += thing.sightAngle;
+        sightLength += thing.sightLength;
+    })
+    speed /= things.length;
+    agility /= things.length;
+    size /= things.length;
+    storage /= things.length;
+    sightAngle /= things.length;
+    sightLength /= things.length;
+    
+    return [speed, agility, size, storage, sightAngle, sightLength];
+}
+/////////////////////////////////////////////////////////////INPUTS//////////////////////////////////////////
+var addEvent = document.addEventListener ? function (target, type, action) {
+    if (target) {
+        target.addEventListener(type, action, false);
+    }
+} : function (target, type, action) {
+    if (target) {
+        target.attachEvent('on' + type, action, false);
+    }
+}
+
+addEvent(document, 'keydown', function (e) {
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+    if (key === 87) {
+        OnW();
+    }
+    if (key === 83) {
+        OnS();
+    }
+    if (key === 81) {
+        OnQ();
+    }
+    if (key === 65) {
+        OnA();
+    }
+    if (key === 68) {
+        OnD();
+    }
+    if (key === 69) {
+        OnE();
+    }
+});
+var cameraMoveSpeed = 5;
+function OnW() {
+    scene.cameraPos.y -= cameraMoveSpeed;
+}
+function OnA() {
+    scene.cameraPos.x -= cameraMoveSpeed;
+}
+function OnS() {
+    scene.cameraPos.y += cameraMoveSpeed;
+}
+function OnD() {
+    scene.cameraPos.x += cameraMoveSpeed;
+}
+function OnQ() {
+    scene.cameraCast -= cameraMoveSpeed;
+}
+function OnE() {
+    scene.cameraCast += cameraMoveSpeed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //LOGIC
+Start();
 setInterval(function () {
+    Update();
     scene.Update();
-}, 50);
+    
+}, 1 / frameRate);
